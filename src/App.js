9b6f68aef2be3fbbe9f31f2f1af902e8783c27ca
Colby Price <colbywprice@gmail.com>
2020-11-06 17:00:08 -0600

    Update 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import Task from './components/Task';
+import Input from './components/Input';
 
 function App() {
+  const [list, setList] = useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container" style={{width: "800px"}}>
+      {list.map((task, i) => (
+        <Task task={task} setList={setList} index ={i} list={list} />
+      ))}
+      <Input list={list} setList={setList}/>
     </div>
   );
 }
